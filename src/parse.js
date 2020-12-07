5a36d8ae96d7f34785b0cb5d92562f45f0c95bdb
wang.whr <wang.whr@alibaba-inc.com>
2020-12-07 16:58:08 +0800

    优化代码

--- src/parse.js
+++ src/parse.js
@@ -1,124 +1,121 @@
-function parse(typeStr){
-    let p = 0;
-    const p2 = 0;
+function parse(typeStr) {
     let slip = 0;
-    let sss = 1;
 
-    function getNextWord(onlyGet=false) {
+    function getNextWord(onlyGet = false) {
         const beginSlip = slip;
-        const keywords = ['[]','[', ']','|',',','<','>','{','}','?:',':','?'];
+        const keywords = ['[]', '[', ']', '|', ',', '<', '>', '{', '}', '?:', ':', '?'];
         let word = typeStr[slip];
-        if(keywords.includes(typeStr.slice(slip,slip+2))){
-            word = typeStr.slice(slip,slip+2);
-            if(!onlyGet){
-                slip+=2
+        if(word === undefined) {
+            return word;
+        }
+        if(keywords.includes(typeStr.slice(slip, slip + 2))) {
+            word = typeStr.slice(slip, slip + 2);
+            if(!onlyGet) {
+                slip += 2
             }
             return word;
-        } else if(keywords.includes(typeStr[slip])){
-            if(!onlyGet){
+        } else if(keywords.includes(typeStr[slip])) {
+            if(!onlyGet) {
                 slip++
             }
             return word
         }
-        while(slip < typeStr.length && p++ < 10000) {
-            if(keywords.includes(typeStr[slip+1])){
+        while (slip < typeStr.length) {
+            if(keywords.includes(typeStr[slip + 1])) {
                 slip++;
-                if(onlyGet){
+                if(onlyGet) {
                     slip = beginSlip;
                 }
                 return word;
             } else {
                 slip++;
             }
-            if(slip < typeStr.length){
+            if(slip < typeStr.length) {
                 word += typeStr[slip];
             }
         }
-        if(onlyGet){
+        if(onlyGet) {
             slip = beginSlip;
         }
         return word;
     }
-    function result(env){
-//         console.log(getNextWord());console.log(getNextWord());console.log(getNextWord());console.log(getNextWord());console.log(getNextWord());console.log(getNextWord());
-//         console.log('*******');
-//         return;
+
+    function result(env) {
         let temp = {};
         const word = getNextWord(true)
-        if(word===undefined){
+        if(word === undefined) {
             return undefined
         }
-        if(['number','string','boolean','any','void','undefined','null','never','object'].includes(getNextWord(true))){
-            temp = {type:getNextWord()}
-        } else if(getNextWord(true)==='Array'){
+        if(['number', 'string', 'boolean', 'any', 'void', 'undefined', 'null', 'never', 'object'].includes(getNextWord(true))) {
+            temp = {type: getNextWord()}
+        } else if(getNextWord(true) === 'Array') {
             getNextWord()
-            if(getNextWord()==='<') {
+            if(getNextWord() === '<') {
                 temp = {
-                    type:'array',
+                    type: 'array',
                     value: result('globle')
                 }
             }
-            if(getNextWord()==='>'){
+            if(getNextWord() === '>') {
             }
-        } else if(getNextWord(true)==='[') {
+        } else if(getNextWord(true) === '[') {
             slip++;
-            sss = 0
-            temp = {type:'tuple',value:[]}
-            while(sss++<10){
+            temp = {type: 'tuple', value: []}
+            while (getNextWord(true) !== undefined) {
                 const orTemp = result('globle')
                 temp.value.push(orTemp)
                 const nextTemp = getNextWord(true);
                 slip++;
-                if(nextTemp!==','){
+                if(nextTemp !== ',') {
                     break;
                 }
             }
-        } else if(getNextWord(true)==='{'){
+        } else if(getNextWord(true) === '{') {
             slip++;
-            temp = {type:'object',value:[]}
-            sss = 0;
-            while(sss++<10){
+            temp = {type: 'object', value: []}
+            while (getNextWord(true) !== undefined) {
                 let key = getNextWord();
-                let mastNeed = getNextWord()!=='?:';
+                let mastNeed = getNextWord() !== '?:';
                 let value = result('globle');
                 const nextWord = getNextWord()
                 temp.value.push({
-                    type:'objectValue',
-                    mastNeed:mastNeed,
-                    key:key,
-                    value:value
+                    type: 'objectValue',
+                    mastNeed: mastNeed,
+                    key: key,
+                    value: value
                 })
-                if(nextWord==='}'){
+                if(nextWord === '}') {
                     break;
                 }
             }
         }
-        if(getNextWord(true)==='|'){
+        if(getNextWord(true) === '|') {
             slip++;
-            sss = 0
-            while(sss++<10){
+            while (getNextWord(true) !== undefined) {
                 const orTemp = result('globle')
-                if(orTemp.type==='|'){
+                if(orTemp.type === '|') {
                     orTemp.value.push(temp);
                     temp = orTemp;
                 } else {
-                    temp = {type:'|',value:[temp,orTemp]}
+                    temp = {type: '|', value: [temp, orTemp]}
                 }
-                if(getNextWord(true)!=='|'){
+                if(getNextWord(true) !== '|') {
                     break;
                 }
             }
-        } else if(getNextWord(true)==='[]'){
-            while(getNextWord(true)==='[]'){
-                slip+=2;
+        } else if(getNextWord(true) === '[]') {
+            while (getNextWord(true) === '[]') {
+                slip += 2;
                 temp = {
-                    type:'array',
-                    value:temp
+                    type: 'array',
+                    value: temp
                 }
             }
         }
         return temp;
     }
+
     return result('globle')
 }
+
 exports.parse = parse;
