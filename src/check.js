e3c4ed86f74eac934eac72ce558ed4df5a9cc28b
wang.whr <wang.whr@alibaba-inc.com>
2020-12-07 17:01:44 +0800

    优化代码

--- src/check.js
+++ src/check.js
@@ -1,17 +1,17 @@
 function check(parseConfig, value) {
     function _checkConfig(config, val) {
-        if (config.type === 'number') {
+        if(config.type === 'number') {
             return typeof val === 'number'
-        } else if (config.type === 'string') {
+        } else if(config.type === 'string') {
             return typeof val === 'string'
-        } else if (config.type === 'boolean') {
+        } else if(config.type === 'boolean') {
             return typeof val === 'boolean'
-        } else if (config.type === 'void') {
+        } else if(config.type === 'void') {
             return val === null || val === undefined;
-        } else if (config.type === 'array') {
-            if (val instanceof Array) {
+        } else if(config.type === 'array') {
+            if(val instanceof Array) {
                 for (let i = 0; i < val.length; i++) {
-                    if (_checkConfig(config.value, val[i]) === false) {
+                    if(_checkConfig(config.value, val[i]) === false) {
                         return false;
                     }
                 }
@@ -19,43 +19,43 @@ function check(parseConfig, value) {
             } else {
                 return false;
             }
-        } else if (config.type === '|') {
+        } else if(config.type === '|') {
             for (let i = 0; i < config.value.length; i++) {
-                if (_checkConfig(config.value[i], val) === true) {
+                if(_checkConfig(config.value[i], val) === true) {
                     return true;
                 }
             }
             return false;
-        } else if (config.type === 'tuple') {
-            if (!(val instanceof Array) || config.value.length !== val.length) {
+        } else if(config.type === 'tuple') {
+            if(!(val instanceof Array) || config.value.length !== val.length) {
                 return false;
             }
             for (let i = 0; i < config.value.length; i++) {
-                if (_checkConfig(config.value[i], val[i]) === false) {
+                if(_checkConfig(config.value[i], val[i]) === false) {
                     return false;
                 }
             }
             return true;
-        } else if (config.type === 'object') {
-            if (typeof val !== 'object') {
+        } else if(config.type === 'object') {
+            if(typeof val !== 'object') {
                 return false;
             }
             const configKeys = config.value.map(item => item.key);
-            if (Object.keys(val).find(key => {
+            if(Object.keys(val).find(key => {
                 return !configKeys.includes(key)
             })) {
                 return false;
             }
             for (let i = 0; i < config.value.length; i++) {
                 let temp = config.value[i];
-                if (val[temp.key] === undefined) {
-                    if (temp.mastNeed) {
+                if(val[temp.key] === undefined) {
+                    if(temp.mastNeed) {
                         return false;
                     } else {
                         return true;
                     }
                 }
-                if (_checkConfig(temp.value, val[temp.key]) === false) {
+                if(_checkConfig(temp.value, val[temp.key]) === false) {
                     return false;
                 }
             }
