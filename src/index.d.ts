d3a1f0048c946f50e9246b5091d19202fe4bde60
wang.whr <wang.whr@alibaba-inc.com>
2021-02-10 11:34:49 +0800

    增加了TS描述

---
+++ src/index.d.ts
@@ -0,0 +1,7 @@
+export declare function parse(typeStr: string);
+
+export declare function check(option: string, value: any);
+
+export declare function checkByOption(option: any, value: any);
+
+export declare function stringify(config: any);
